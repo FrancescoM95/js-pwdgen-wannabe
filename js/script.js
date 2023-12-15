6a22d0f2f63e60d0fcfc2eb907d3ad62c4d66234
FrancescoM95 <mon95fran@gmail.com>
2023-12-15 16:29:22 +0100

    asked lastname

--- js/script.js
+++ js/script.js
@@ -5,4 +5,7 @@ console.log('JS OK')
 const firstname = prompt('Come ti chiami?');
 console.log(firstname);
 
+//* 2. Chiediamo all'utente il suo cognome. 
 
+const lastname = prompt('Qual è il tuo cognome?');
+console.log(lastname);
